9b67842ed9003a1cbc0167d24316442944ee1fc3
chinawiz <chinawiz@gmail.com>
2019-07-16 17:42:26 +0000

    关注用户动态流

--- app/Models/User.php
+++ app/Models/User.php
@@ -59,7 +59,10 @@ class User extends Authenticatable
 
     public function feed()
     {
-        return $this->statuses()->orderByDesc('created_at');
+        $user_ids = $this->followings->pluck('id')->toArray();
+        array_push($user_ids, $this->id);
+        dd($user_ids);
+        return Status::whereIn('user_id', $user_ids)->with('user')->orderBy('created_at','desc');
     }
 
     public function followers()
@@ -82,14 +85,14 @@ class User extends Authenticatable
 
     public function unfollow($user_ids)
     {
-        if (!is_array($user_ids)){
+        if (!is_array($user_ids)) {
             $user_ids = compact('user_ids');
         }
         $this->followings()->detach($user_ids);
     }
 
-    public function is_following($user_id)
+    public function isFollowing($user_id)
     {
-        return $this->followings()->contains($user_id);
+        return $this->followings->contains($user_id);
     }
 }
